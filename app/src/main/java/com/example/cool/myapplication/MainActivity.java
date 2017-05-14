5f45cdb44119456c3a219c2bb1e63acdef353e12
WWcodeTW <f9988@ocr.tw>
2017-05-14 16:09:08 +0800

    init

---
+++ app/src/main/java/com/example/cool/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cool.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WebView webView = (WebView) findViewById(R.id.WebView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.loadUrl("file:///android_asset/index3.html");
+        }
+
+}
+
